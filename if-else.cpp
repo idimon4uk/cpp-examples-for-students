03d6ea21728b43b67264beb0c008e7e0c6ec472d
user-pc\Dima <dimqqqq@gmail.com>
2019-01-24 17:47:06 +0200

    init

---
+++ if-else.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	///example with if-else
+	///Life-sycle category detect
+	///according to https://www.statcan.gc.ca/eng/concepts/definitions/age2
+	
+	int age;
+	int children_age = 14;
+	char* category;
+	cout<<"- How old are you? \n- I'm ";
+	cin>>age;
+	
+	///simple
+	if(age<=children_age){
+		category = "children";
+	}
+	else{
+		category = "not children";
+	}
+	cout<<"- You are "<<category<<endl;
+	
+	///simpler way
+	
+	cout<<"- You are "<<((age>14)?"not ":"")<<"children"<<endl;
+	
+	///advanced
+	children_age = 14;
+	int youth_age = 24;
+	int adults_age = 64;
+	
+	if(age>adults_age){
+		category = "senior";
+	}
+	else if(age>youth_age){
+		category = "adults";
+	}
+	else if(age>children_age){
+		category = "youth";
+	}
+	else {
+		category = "children";
+	}
+	
+	cout<< "- You belong to the category "<<category<<endl;
+	 
+	
+	///example with switch-case
+	double number1=0, number2=0, result=0;
+	char operator_;
+	cout<<"You have equation of type : a _ b = c"<<endl;
+	cout<<"enter the value instead of 'a': ";
+	cin>>number1;
+	cout<<"enter the operator instead of '_' ('+','-','*','/'): ";
+	cin>>operator_;
+	cout<<"enter the value instead of 'b'";
+	cin>>number2;
+	
+	switch(operator_){
+		case '+':{
+			result = number1+number2;
+			break;
+		}
+		case '-':{
+			result = number1-number2;
+			break;
+		}
+		case '*':{
+			result = number1*number2;
+			break;
+		}
+		case '/':{
+			result = number1/number2;
+			break;
+		}
+		default:{
+			cout<<"Enter correct operrator";
+			break;
+		}
+	}
+	cout<<number1<<operator_<<number2<<"="<<result<<endl;
+	
+	
+	system("PAUSE");
+	return 0;
+}
---
+++ loop.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	///Fibonacci number
+	///According to https://en.wikipedia.org/wiki/Fibonacci_number
+	
+	///do-while implementation	
+	char isRepeat = 'n';
+	do {
+		
+		/// for-loop implementation example
+		int n;
+		cout<<"enter n :";
+		cin>>n;
+		int buffer;
+		int f0 = 0, f1 = 1;
+	
+		for(int i=0;i<n;i++){
+			buffer = f1;
+			f1=f1+f0;
+			f0=buffer;
+		}
+		cout<<"f("<<n<<")="<<f0<<endl;
+	
+		///while-loop implementation example
+	
+		f0 = 0;
+		f1 = 1;
+		int i = 0;
+		while(i<n){
+			buffer = f1;
+			f1=f1+f0;
+			f0=buffer;
+			i++;
+		}
+	
+		cout<<"f("<<n<<")="<<f0<<endl;
+		
+		cout<<"repeat one more time? y/n :";
+		cin>>isRepeat;
+	}
+	while(isRepeat=='y');
+	
+	system("PAUSE");
+	return 0;
+}
